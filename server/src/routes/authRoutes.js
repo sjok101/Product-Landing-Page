0ccdcd59dbf8182bbbdc16847b1aa201b6a01c5a
sjok <sjok101@gmail.com>
2024-12-27 03:43:34 -0500

    removed . from route address

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 
 
 const { createUser, loginUser } = require('../controller/authController');
-router.post('./signup', createUser);
-router.post('./login', loginUser);
+router.post('/signup', createUser);
+router.post('/login', loginUser);
 module.exports = router;
