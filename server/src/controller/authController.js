b825b6830662ab80d2dc2cb6a7949f229725306b
sjok <sjok101@gmail.com>
2024-12-27 03:35:18 -0500

    renamed createUser to loginUser and then added logic for createUser

--- server/src/controller/authController.js
+++ server/src/controller/authController.js
@@ -1,7 +1,25 @@
 const User = require('../models/User');
 
 exports.createUser = async(req, res) => {
-    const { username, password } = req.body;
+    const { username, email, password } = req.body;
+
+    try{
+        const existingUser = await User.findOne({email});
+        if(existingUser) {
+            return res.status(404).json({ error: 'User already exists' });
+        }
+
+        const user = new User({ username, email, password });
+        await user.save();
+
+        res.status(201).json({ message: 'User created successfully.'});
+    }catch(error){
+        res.status(500).json({message: `${error}`});
+    }
+}
+
+exports.loginUser = async(req, res) => {
+    const { username, email, password } = req.body;
 
     try{
         const user = await User.findOne({username});
