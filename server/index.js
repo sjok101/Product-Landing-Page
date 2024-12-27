f54b22ea2b58652c6e64ecb3e694602ba6e6478a
sjok <sjok101@gmail.com>
2024-12-27 03:34:22 -0500

    added route to index

--- server/index.js
+++ server/index.js
@@ -12,6 +12,10 @@ app.use(cors());
 
 connectMongo();
 
+const authRoute = require("./src/routes/authRoutes");
+app.use('/user', authRoute)
+
+
 const PORT = process.env.PORT || 3030;
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
