65ab11abe11c34fec191daa2ed88432ce0d0c82b
sjok <sjok101@gmail.com>
2024-12-27 03:36:39 -0500

    added loginUser to import param

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 
-const { createUser } = require('../controller/authController');
+const { createUser, loginUser } = require('../controller/authController');
 router.post('./signup', createUser);
 router.post('./login', loginUser);
 module.exports = router;
