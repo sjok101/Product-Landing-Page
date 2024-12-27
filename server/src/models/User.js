5c3fd3b214054d59902561ce975a15314be0e2c7
sjok <sjok101@gmail.com>
2024-12-27 03:08:35 -0500

    Add pre-hash for password

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -17,5 +17,18 @@ const userSchema = new mongoose.Schema({
 
 })
 
+userSchema.pre('save', async (next) =>  {
+    //prevent re-hashing on updates
+    if (!this.isModified('password')) return next();
+
+    try{
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    }catch(error){
+        next(error);
+    }
+});
+
 const User = mongoose.model('User', userSchema);
 module.exports = User;
