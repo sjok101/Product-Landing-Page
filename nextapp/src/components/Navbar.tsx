7ea85a681fc6e4896d8ebc04cb44eddd8b7a7ed7
sjok <sjok101@gmail.com>
2025-04-18 14:55:02 -0400

    add spacing

--- nextapp/src/components/Navbar.tsx
+++ nextapp/src/components/Navbar.tsx
@@ -5,12 +5,12 @@ const Navbar = () => {
     <div>
       <div>
         <div id="nav-links" className="flex flex-row fixed ">
-          <div id="nav-links-left">
+          <div id="nav-links-left" className="space-x-2">
             <a href="/">Link 1</a>
             <a href="/">Link 2</a>
             <a href="/">Link 3</a>
           </div>
-          <div id="nav-links-right" className="fixed right-0">
+          <div id="nav-links-right" className="fixed right-0 space-x-2">
             <a href="/">Login</a>
             <a href="/">Sign-up</a>
           </div>
