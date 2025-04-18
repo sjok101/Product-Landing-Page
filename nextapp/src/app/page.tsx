f19c8dc77e3c3616811ff1df411338f9f7ddfce9
sjok <sjok101@gmail.com>
2025-04-18 14:03:48 -0400

    clean up page structure

--- nextapp/src/app/page.tsx
+++ nextapp/src/app/page.tsx
@@ -11,26 +11,12 @@ export default function Home(){
 
     return(
         <div>
-            <div className ="nav-container">
             <Navbar/>
-            </div>
-        <div id="landing-container">
-            <div className ="section-container">
             <Intro/>
-            </div>
-            <div className ="section-container">
             <Why/>
-            </div>
-            <div className ="section-container">
             <How/>
-            </div>
-            <div className ="section-container">
             <Pricing/>
-            </div>
-            <div className ="section-container">
             <Footer/>
-            </div>
-        </div>
         </div>
     );
 }
