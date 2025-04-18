b308621a13f031a007a481729a14159b554f316f
sjok <sjok101@gmail.com>
2025-04-18 14:54:09 -0400

    add basic flex styles

--- nextapp/src/components/Footer.tsx
+++ nextapp/src/components/Footer.tsx
@@ -1,29 +1,30 @@
-
+import Image from 'next/image';
 function Footer(){
+    const linkStyles = 'flex flex-col'
     return(
         <div>
-        <div id="footer-box" className="bg-lime-200">
+        <div id="footer-box" className="bg-lime-200 flex flex-col justify-center items-center space-y-10">
             <h1>Footer</h1>
-            <div id= "footer-link-container">
-                <div className = "footer-link-column">
+            <div id= "footer-link-container" className='flex flex-row space-x-3'>
+                <div className = {linkStyles}>
                     <a href="">[Footer Link]</a>
                     <a href="">[Footer Link]</a>
                     <a href="">[Footer Link]</a>
                     <a href="">[Footer Link]</a>
                 </div>
-                <div className = "footer-link-column">
+                <div className = {linkStyles}>
                     <a href="">[Footer Link]</a>
                     <a href="">[Footer Link]</a>
                     <a href="">[Footer Link]</a>
                     <a href="">[Footer Link]</a>
                 </div>
-                <div className = "footer-link-column">
+                <div className = {linkStyles}>
                     <a href="">[Footer Link]</a>
                     <a href="">[Footer Link]</a>
                     <a href="">[Footer Link]</a>
                     <a href="">[Footer Link]</a>
                 </div>
-                <div className = "footer-link-column">
+                <div className = {linkStyles}>
                     <a href="">[Footer Link]</a>
                     <a href="">[Footer Link]</a>
                     <a href="">[Footer Link]</a>
@@ -32,11 +33,11 @@ function Footer(){
                 </div>
             </div>
 
-            <div id="footer-social-media-links">
-                <a target="_blank" href="https://facebook.com"><img src="facebook-small.png"></img></a>
-                <a target="_blank" href="https://instagram.com"><img src="instagram-small.png"></img></a>
-                <a target="_blank" href="https://x.com"><img src="twitter-small.png"></img></a>
-                <a target="_blank" href="https://youtube.com"><img src="youtube-small.png"></img></a>
+            <div id="footer-social-media-links" className="flex flex-row">
+                <a target="_blank" href="https://facebook.com"><Image src="/facebook-small.png" alt='facebook' width={40} height={40}></Image></a>
+                <a target="_blank" href="https://instagram.com"><Image src="/instagram-small.png" alt='instagram' width={40} height={40}></Image></a>
+                <a target="_blank" href="https://x.com"><Image src="/twitter-small.png" alt='twitter' width={40} height={40}></Image></a>
+                <a target="_blank" href="https://youtube.com"><Image src="/youtube-small.png" alt='youtube' width={40} height={40}></Image></a>
             </div>
 
 
