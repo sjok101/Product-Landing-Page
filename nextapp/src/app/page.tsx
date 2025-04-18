e4b221d0244fb211e103da50b6c38cf5a3dc8c86
sjok <sjok101@gmail.com>
2025-04-18 13:54:02 -0400

    Remove useEffect, useState

--- nextapp/src/app/page.tsx
+++ nextapp/src/app/page.tsx
@@ -6,32 +6,9 @@ import Why from "../components/Why";
 import How from "../components/How";
 import Pricing from "../components/Pricing";
 import Footer from "../components/Footer";
-import { useEffect, useState} from 'react';
-
-function dynamicScale() {
-    const containers = document.querySelectorAll('.section-container');
-    const scale = window.innerHeight/1000;
-    console.log(scale)
-    
-    if (scale<1.0&&scale>0.5){
-    containers.forEach( container => {
-        container.style.height = `${750/scale}px`;
-
-    })
-    }
-}
 
 export default function Home(){
-    useEffect(() => {
-        dynamicScale();
 
-        function handleResize() {
-            dynamicScale(); // Scale on window resize
-          }
-
-          window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
     return(
         <div>
             <div className ="nav-container">
