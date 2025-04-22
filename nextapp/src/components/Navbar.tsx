03349f76b6f6df6c5bcad9b9b2bb4d618b1b305d
sjok <sjok101@gmail.com>
2025-04-22 17:07:56 -0400

    replaced nav placeholders

--- nextapp/src/components/Navbar.tsx
+++ nextapp/src/components/Navbar.tsx
@@ -6,13 +6,13 @@ const Navbar = () => {
       <div>
         <div id="nav-links" className="flex flex-row fixed ">
           <div id="nav-links-left" className="space-x-2">
-            <a href="/">Link 1</a>
-            <a href="/">Link 2</a>
-            <a href="/">Link 3</a>
+            <a href="/">Features</a>
+            <a href="/">Pricing</a>
+            <a href="/">Demo</a>
           </div>
           <div id="nav-links-right" className="fixed right-0 space-x-2">
-            <a href="/">Login</a>
-            <a href="/">Sign-up</a>
+            <a href="/">FAQ</a>
+            <a href="/">Contact</a>
           </div>
         </div>
       </div>
