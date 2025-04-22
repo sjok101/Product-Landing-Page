7be9cbc46b486e10ad15fc1a7c3e9470966f2215
sjok <sjok101@gmail.com>
2025-04-22 17:24:14 -0400

    add features page

--- nextapp/src/app/features/page.tsx
+++ nextapp/src/app/features/page.tsx
@@ -1,7 +1,10 @@
+import Navbar from "@/components/Navbar";
+
 export default function Features(){
     return(
         <div>
-            features
+            <Navbar></Navbar>
+            
         </div>
     )
 }
